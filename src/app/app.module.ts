f9f2d95513a7cd0998d93203005decdc3a636840
unknown <adayesi>
2021-10-16 12:59:00 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { PersonalPortFolioComponent } from './personal-port-folio/personal-port-
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { RecentProjectsComponent } from './recent-projects/recent-projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PersonalPortFolioComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    RecentProjectsComponent
   ],
   imports: [
     BrowserModule,
